41992adf228089f7d3158ed12a5ffe2efd8a8e54
bluetie915 <1402711057@qq.com>
2019-09-21 09:49:47 +0800

    delete out

--- out/production/community/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table USER
-(
-  ID INT auto_increment primary key not null,
-  ACCOUNT_ID   VARCHAR(100),
-  NAME         VARCHAR(50),
-  TOKEN        CHAR(36),
-  GMT_CREATE   BIGINT,
-  GMT_MODIFIED BIGINT
-);
