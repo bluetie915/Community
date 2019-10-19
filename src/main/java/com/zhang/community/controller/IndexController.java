73bc7e4332643768a745af2e16b66f709cc5a3df
bluetie915 <1402711057@qq.com>
2019-10-19 22:22:59 +0800

    完善我的问题页面

--- src/main/java/com/zhang/community/controller/IndexController.java
+++ src/main/java/com/zhang/community/controller/IndexController.java
@@ -1,17 +1,12 @@
 package com.zhang.community.controller;
 
 import com.zhang.community.dto.PaginationDTO;
-import com.zhang.community.mapper.UserMapper;
-import com.zhang.community.model.User;
 import com.zhang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
