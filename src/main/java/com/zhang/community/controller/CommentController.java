b5ff55fb4ba7d088bb278aabb6e76b2d8429e78d
bluetie915 <1402711057@qq.com>
2019-10-24 18:52:47 +0800

    完善评论系统

--- src/main/java/com/zhang/community/controller/CommentController.java
+++ src/main/java/com/zhang/community/controller/CommentController.java
@@ -1,14 +1,20 @@
 package com.zhang.community.controller;
 
 import com.zhang.community.dto.CommentDTO;
+import com.zhang.community.dto.ResultDTO;
 import com.zhang.community.mapper.CommentMapper;
 import com.zhang.community.model.Comment;
+import com.zhang.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class CommentController {
@@ -18,7 +24,13 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO) {
+    public Object post(@RequestBody CommentDTO commentDTO,
+                       HttpServletRequest request) {
+
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null){
+            return ResultDTO.errorOf(2002,"未登录不能进行评论，请先登录");
+        }
         Comment comment = new Comment();
         comment.setParentId(commentDTO.getParentId());
         comment.setContent(commentDTO.getContent());
@@ -28,6 +40,8 @@ public class CommentController {
         comment.setCommentator(1);
         comment.setLikeCount(0L);
         commentMapper.insert(comment);
-        return null;
+        Map<Object, Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("message", "成功");
+        return objectObjectHashMap;
     }
 }
